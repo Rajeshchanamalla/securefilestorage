3b2d7b44ccb2d8ee6eac3fc66a9dc4a80a99eea6
Rajeshchanamalla <chinnur187@gmail.com>
2025-03-29 16:20:51 +0530

    commitchanges

--- file_crypto/views.py
+++ file_crypto/views.py
@@ -1,6 +1,9 @@
 import os
+import subprocess
+import sys
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db import models
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.decorators import login_required
 from .aes_crypto import aes_encrypt, aes_decrypt
@@ -9,13 +12,19 @@ from .blowfish_crypto import blowfish_encrypt, blowfish_decrypt
 from .hybrid_crypto import hybrid_encrypt, hybrid_decrypt
 from .models import DeletedFile, EncryptionHistory
 
+
+# Get the project base directory dynamically
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-KEYS_DIR = os.path.join(BASE_DIR, "keys")
+KEYS_DIR = os.path.join(BASE_DIR, "keys")  # Ensure keys are stored in the project folder
 PUBLIC_KEY_PATH = os.path.join(KEYS_DIR, "public_key.pem")
 PRIVATE_KEY_PATH = os.path.join(KEYS_DIR, "private_key.pem")
-ATTEMPT_LIMIT = 3
+ATTEMPT_LIMIT = 3  # Maximum allowed password attempts
 
+# Ensure the `keys` directory exists
 os.makedirs(KEYS_DIR, exist_ok=True)
+
+
+# Dictionary to track failed attempts
 failed_attempts = {}
 
 def ensure_rsa_keys():
@@ -101,3 +110,12 @@ def decrypt_file(request):
     except Exception as e:
         messages.error(request, f"An unexpected error occurred: {e}")
         return redirect("decrypt")
+
+@login_required
+def history(request):
+    encrypt_history = EncryptionHistory.objects.filter(user=request.user, action_type='ENCRYPT').order_by('-timestamp')
+    decrypt_history = EncryptionHistory.objects.filter(user=request.user, action_type='DECRYPT').order_by('-timestamp')
+    return render(request, "history.html", {
+        "encrypt_history": encrypt_history,
+        "decrypt_history": decrypt_history
+    })
